e7c5b649899c6da80ae8c01b8778834082fe2ae6
simoncarriere <simoncarriere@icloud.com>
2022-04-15 10:41:25 +0545

    Environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDcuvu0auS8Vi3LLAV7tU0XWtkL0SALQ2M",
-    authDomain: "udemy-financetracker.firebaseapp.com",
-    projectId: "udemy-financetracker",
-    storageBucket: "udemy-financetracker.appspot.com",
-    messagingSenderId: "182020576541",
-    appId: "1:182020576541:web:3cfa257ec1c9777cabfdf4",
-    measurementId: "G-YXF66T1FZX"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY, 
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
 
   //init firebase
